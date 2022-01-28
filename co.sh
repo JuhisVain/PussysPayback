6eaa462aade316c7462d742918b8b284f2105a9a
JuhisVain <Juho.Vainio@protonmail.ch>
2022-01-28 12:15:40 +0200

    warnings

--- co.sh
+++ co.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=./blt/
-gcc src/main.c src/world.c src/cat.c src/game.c blt/libBearLibTerminal.so
+gcc -Wall src/main.c src/world.c src/cat.c src/game.c blt/libBearLibTerminal.so
