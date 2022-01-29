89f7427e6719f21aee8f4eb3dabf6bed9824c6df
JuhisVain <Juho.Vainio@protonmail.ch>
2022-01-29 11:43:01 +0200

    Optimization flag

--- co.sh
+++ co.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=./blt/
-gcc -Wall src/main.c src/world.c src/cat.c src/game.c blt/libBearLibTerminal.so
+gcc -Wall -O3 src/main.c src/world.c src/cat.c src/game.c blt/libBearLibTerminal.so
