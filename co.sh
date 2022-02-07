edcccb4cfa06e5d6b942e30e15e0d08c5e63fcbc
JuhisVain <Juho.Vainio@protonmail.ch>
2022-02-07 11:40:15 +0200

    Add render queue

--- co.sh
+++ co.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=./blt/
-gcc -Wall -O3 src/main.c src/util.c src/world.c src/cat.c src/game.c src/doggy.c blt/libBearLibTerminal.so
+gcc -Wall -O3 src/main.c src/util.c src/world.c src/cat.c src/game.c src/doggy.c src/render_queue.c blt/libBearLibTerminal.so
