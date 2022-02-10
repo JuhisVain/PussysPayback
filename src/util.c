077a33478ae3aa24786a2c795b592decc751e6bd
JuhisVain <Juho.Vainio@protonmail.ch>
2022-02-10 11:04:43 +0200

    Coordinate distance func

--- src/util.c
+++ src/util.c
@@ -1,5 +1,11 @@
+#include "util.h"
 
 int sign(int num)
 {
     return (num<0)?-1:(num>0)?1:0;
+}
+
+double distance(int ox, int oy, int tx, int ty)
+{
+    return sqrt(((tx-ox)^2) + ((ty-oy)^2));
 }
--- src/util.h
+++ src/util.h
@@ -1,6 +1,9 @@
 #ifndef UTILH
 #define UTILH
 
+#include <math.h>
+
 int sign(int num);
+double distance(int ox, int oy, int tx, int ty);
 
 #endif
